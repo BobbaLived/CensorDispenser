a1858a424813f8dea9dadd3435a1638d3130471c
Jacob L <jacob.j.lee43@gmail.com>
2020-07-08 01:16:19 -0400

    Finalized censor program

--- censor_dispenser-starting/censor_dispenser/censor_dispenser.py
+++ censor_dispenser-starting/censor_dispenser/censor_dispenser.py
@@ -33,42 +33,25 @@ def censor3(email, words):
             if k >= 2:
                 email = email.replace(n, "*****")
     return email
-#Right now this isn't working as intended.  Is censoring every word, not only after second occurence.  Unsure how to fix at the moment.
+#TODO Right now this isn't working as intended.  Is censoring every word, not only after second occurence.  Unsure how to fix at the moment.
 
 #Testing Req 4
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
-print(censor3(email_three,negative_words))
+#print(censor3(email_three,negative_words))
 
-#Requirement #5
-def censor4(email, lst1, lst2):
-    for n in lst1:                                              #For each word in lst1
-        if n in email:                                          #If that word is in the email
-            pos = email.find(n)                                 #Find the position of the first letter in that word in the email
-            pos1 = pos - 2                                      #Now, pos1 is the leftward start of my substring.  Go back two from the first letter of the word
-            pos2 = pos + len(n) +1                              #pos2 is my rightward bound of my substring.  Go ahead to the end of the word and beyond the space following
-            while email[pos1] != " ":                           #Now, whenever I run into a string again, give me that position (leftward here)
-                pos1 += -1
-            while email[pos2] != " ":
-                pos2 += 1
-        temp = email[pos1:pos2]                                 #Now, create a temporary string of the substring here
-        email = email.replace(temp, " CENSOR")                  #Now censor that substring out of the email
+def censor4(email, lst1):
+    temp = email.split(" ")
+    for n in lst1:
+        for i, k in enumerate(temp):
+            if n in k:
+                temp[i] = "CENSOR"
+                if i > 1:
+                    temp[i - 1] = ""
+                if i < len(temp) - 1:
+                    temp[i+1] = ""
+    return " ".join(temp)
 
-    # NOW DO THE EXACT SAME THING BUT FOR LST2!
-
-    for n in lst2:
-        if n in email:                                          #If that word is in the email
-            pos = email.find(n)                                 #Find the position of the first letter in that word in the email
-            pos1 = pos - 2                                      #Now, pos1 is the leftward start of my substring.  Go back two from the first letter of the word
-            pos2 = pos + len(n) +1                              #pos2 is my rightward bound of my substring.  Go ahead to the end of the word and beyond the space following
-            while email[pos1] != " ":                           #Now, whenever I run into a string again, give me that position (leftward here)
-                pos1 += -1
-            while email[pos2] != " ":
-                pos2 += 1
-        temp = email[pos1:pos2]                                 #Now, create a temporary string of the substring here
-        email = email.replace(temp, " CENSOR")
-
-    return email
 #Test requirement #5
-print(censor4(email_four, proprietary_terms, negative_words))
+print(censor4(email_four, proprietary_terms + negative_words))
 
 
